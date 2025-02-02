b83d61c6fae0024c856037843282f96d6f1f1b9a
Kim Boram <tldgkgud00@gmail.com>
2025-02-02 20:57:42 +0900

    Project Build

---
+++ src/test/java/com/fishgo/FishGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
