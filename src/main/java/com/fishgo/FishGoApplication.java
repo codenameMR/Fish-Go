dc6f76ecef1bf1a41b4d28272c4c37dd1ea538b3
Kim Boram <tldgkgud00@gmail.com>
2025-02-02 20:57:42 +0900

    Project Build

---
+++ src/main/java/com/fishgo/FishGoApplication.java
@@ -0,0 +1,13 @@
+package com.fishgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishGoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fishgo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fishgo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FishGoApplication.class);
+	}
+
+}
