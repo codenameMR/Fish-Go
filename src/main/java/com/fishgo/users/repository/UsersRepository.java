98639a9eb664a56b3e75682d0c5b55db5f1122b6
Kim Boram <tldgkgud00@gmail.com>
2025-02-05 13:25:08 +0900

    로그인/회원가입 기본구현

---
+++ src/main/java/com/fishgo/users/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.fishgo.users.repository;
+
+import com.fishgo.users.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUserId(String userId);
+    boolean existsByUserId(String userId);
+}
