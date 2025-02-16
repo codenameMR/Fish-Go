8b9ce633fde632852c4111057c4734dc7ba1aecb
whseo <seou01230@gmail.com>
2025-02-16 19:19:52 +0900

    Posts 수정, 삭제 로직 구현

--- src/main/java/com/fishgo/posts/controller/PostsController.java
+++ src/main/java/com/fishgo/posts/controller/PostsController.java
@@ -26,8 +26,7 @@ public class PostsController {
             Posts savedPost = postsService.createPost(postsDto);
             return ResponseEntity.ok(savedPost);
         } catch (Exception e) {
-            logger.error("############### error : {}", e.getMessage());
-            return ResponseEntity.badRequest().body("실패");
+            return ResponseEntity.badRequest().body("저장실패");
         }
 
     }
@@ -39,6 +38,27 @@ public class PostsController {
             @RequestParam(required = false) String fishType
     ) {
         return postsService.searchPosts(title, hashTag, fishType);
+    }
+
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<?> updatePost(@PathVariable Long postId, @RequestBody PostsDto postsDto) {
+        try {
+            PostsDto updatedPost = postsService.updatePost(postId, postsDto);
+            return ResponseEntity.ok(updatedPost);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("수정실패");
+        }
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable Long postId) {
+        try {
+            postsService.deletePost(postId);
+            return ResponseEntity.ok("삭제되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("삭제실패");
+        }
     }
 
 }
