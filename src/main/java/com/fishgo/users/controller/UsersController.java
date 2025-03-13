afb628c2fe00f00fd83a54a355dbb822497216c8
Kim Boram <tldgkgud00@gmail.com>
2025-03-13 13:05:36 +0900

    - 프로필, 이름 변경 요청 DTO 생성

--- src/main/java/com/fishgo/users/controller/UsersController.java
+++ src/main/java/com/fishgo/users/controller/UsersController.java
@@ -3,6 +3,8 @@ package com.fishgo.users.controller;
 import com.fishgo.common.response.ApiResponse;
 import com.fishgo.users.domain.Users;
 import com.fishgo.users.dto.ProfileResponseDto;
+import com.fishgo.users.dto.UpdateNameRequestDto;
+import com.fishgo.users.dto.UpdateProfileBioRequestDto;
 import com.fishgo.users.dto.UserStatsDto;
 import com.fishgo.users.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,8 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Map;
 
 @Tag(name = "사용자 정보 API", description = "프로필, 기록 등 사용자 정보 관련 API")
 @RestController
@@ -52,16 +52,18 @@ public class UsersController {
 
     @Operation(summary = "프로필 메세지 변경", description = "프로필 메세지를 변경합니다.")
     @PutMapping("/profile-bio")
-    public ResponseEntity<ApiResponse<String>> updateProfileMessage(@RequestBody Map<String, String> payload, @AuthenticationPrincipal Users user) {
-        usersService.updateBio(user, payload.get("bio"));
+    public ResponseEntity<ApiResponse<String>> updateProfileMessage(@RequestBody UpdateProfileBioRequestDto requestDto,
+                                                                    @AuthenticationPrincipal Users user) {
+        usersService.updateBio(user, requestDto.getBio());
 
         return ResponseEntity.ok(new ApiResponse<>("프로필 메세지 변경 성공", HttpStatus.OK.value()));
     }
 
     @Operation(summary = "이름 변경", description = "특수문자 불가능, 최소 2글자, 최대 20글자까지 허용")
     @PutMapping("/name")
-    public ResponseEntity<ApiResponse<String>> updateProfileName(@RequestBody Map<String, String> payload, @AuthenticationPrincipal Users user) {
-        usersService.updateProfileName(user, payload.get("name"));
+    public ResponseEntity<ApiResponse<String>> updateProfileName(@RequestBody UpdateNameRequestDto requestDto,
+                                                                 @AuthenticationPrincipal Users user) {
+        usersService.updateProfileName(user, requestDto.getName());
 
         return ResponseEntity.ok(new ApiResponse<>("사용자 이름 변경 성공", HttpStatus.OK.value()));
     }
---
+++ src/main/java/com/fishgo/users/dto/UpdateProfileBioRequestDto.java
@@ -0,0 +1,10 @@
+package com.fishgo.users.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateProfileBioRequestDto {
+    private String bio;
+}
